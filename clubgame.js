3edb0b8382dd5adc137a6a6dd616a4c18ce62ed0
bruchimp3 <akhileshprasanna@gmail.com>
2023-05-31 19:24:57 +0530

    deposit amount and number of lines bet number

--- clubgame.js
+++ clubgame.js
@@ -1,7 +1,33 @@
 const prompt = require("prompt-sync")();
 
 const deposit = ()=>{
+
+    while(true){
     const depositAmount = prompt("Enter a deposit amount: ");
+    const numberDepositAmount = parseFloat(depositAmount);
+
+    if((isNaN(numberDepositAmount))||(numberDepositAmount<=0)){
+        console.log("Invalid deposit number, Please try again")
+    }else{
+        return numberDepositAmount;
+    }
+}
 };
 
-deposit();
+const getNumberOfLines = ()=>{
+
+    while(true){
+    const lines = prompt("Enter number of lines to bet on (1,2 or 3): ");
+    const numberOfLines = parseFloat(lines);
+
+    if((isNaN(numberOfLines))||(numberOfLines<=0)||(numberOfLines>3)){
+        console.log("Invalid number of lines, Please try again")
+    }else{
+        return numberOfLines;
+    }
+}
+
+}
+
+const depositAmount = deposit();
+const numberOfLines = getNumberOfLines();
